08a543a02ace64b2180ee32bc4721f503d506395
Erin Keenan <erinok@gmail.com>
2016-09-02 21:01:47 -0700

    fix emoji shuffling

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 // Program shufflechars shuffles the characters of N-copies of a string.
 //
-// BUG: doesn't handle the unicode combining things right -- e.g., it'll fuck up the red heart emoji.
+// BUG: just smart enough to handle emoji; likely breaks on fancier unicode stuff (isn't working on the grapheme level like it should).
 package main
 
 import (
@@ -9,6 +9,7 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"unicode"
 )
 
 func main() {
@@ -24,11 +25,31 @@ func main() {
 	for i := 1; i <= n; i++ {
 		runes = append(runes, []rune(os.Args[2])...)
 	}
-	for i := 0; i < len(runes); i++ {
+	// shuffle
+	for i := 0; i < len(runes); {
 		k := rand.Intn(len(runes)-i) + i
-		runes[i], runes[k] = runes[k], runes[i]
+		// we need to move modifier characters as a contiguous unit. we only handle a single modifier correctly.
+		if ismod(runes[k]) && k-1 >= i {
+			runes[i], runes[k-1] = runes[k-1], runes[i]
+			runes[i+1], runes[k] = runes[k], runes[i+1]
+			i += 2
+		} else if k+1 < len(runes) && ismod(runes[k+1]) {
+			runes[i], runes[k] = runes[k], runes[i]
+			runes[i+1], runes[k+1] = runes[k+1], runes[i+1]
+			i += 2
+		} else {
+			runes[i], runes[k] = runes[k], runes[i]
+			i++
+		}
 	}
 	fmt.Println(string(runes))
+	// fmt.Println([]rune(runes))
+}
+
+var mods = []*unicode.RangeTable{unicode.Variation_Selector, unicode.Sk}
+
+func ismod(r rune) bool {
+	return unicode.IsOneOf(mods, r)
 }
 
 func fatal(args ...interface{}) {
